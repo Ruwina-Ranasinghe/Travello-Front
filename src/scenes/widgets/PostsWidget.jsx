889c1de57ca856f711c7422141422e448202e526
Ruwina Ranasinghe <ruvinaranasingha94@gmail.com>
2024-09-28 21:36:09 +0530

    Update PostsWidget.jsx

--- src/scenes/widgets/PostsWidget.jsx
+++ src/scenes/widgets/PostsWidget.jsx
@@ -10,7 +10,7 @@ const PostsWidget = ({ userId, isProfile = false }) => {
 
   const getPosts = async () => {
     try {
-      const response = await fetch("http://localhost:3001/posts", {
+      const response = await fetch("https://travello-backend.vercel.app/posts", {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
       });
@@ -28,7 +28,7 @@ const PostsWidget = ({ userId, isProfile = false }) => {
   const getUserPosts = async () => {
     try {
       const response = await fetch(
-        `http://localhost:3001/posts/${userId}/posts`,
+        `https://travello-backend.vercel.app/posts/${userId}/posts`,
         {
           method: "GET",
           headers: { Authorization: `Bearer ${token}` },
